2c6ed421841943680023aa57a5da9ace5172f94c
Leo Yockey <leoyockey@gmail.com>
2020-09-24 10:12:50 -0700

    additional metadata when modifying document

--- api/routes/config.js
+++ api/routes/config.js
@@ -5,13 +5,16 @@ const mongoose = require('mongoose');
 
 // TODO: move to (and create) "models" directory
 const schema = new mongoose.Schema({
-  auth0Users: [{type: Map, of: String}], // array of objects - userId and email address
-  customerName: String,
-  projectName: String,
   allPropertiesRequired: Boolean,
-  requiredProperties: [String],
+  auth0Users: [{type: Map, of: String}], // array of objects - userId and email address
+  createdAt: String,
+  customerName: String,
+  lastUpdated: String,
+  projectName: String,
   properties: Map,
   readData: Array,
+  requiredProperties: [String],
+  updatedBy: String,
   writeData: Array
 });
 
@@ -36,13 +39,17 @@ const prefix = '/config'
 const WHERE = req => ({'auth0Users.id': req.user.sub === process.env.MACHINE_CLIENT_ID ? req.headers['x-user-id'] : req.user.sub});
 
 // TODO: only allow admins to access this endpoint
-// router.post(prefix, (req, res) => {
-//   const customer = new Config(req.body);
-//   customer.save((err) => {
-//     if (err) return err;
-//   });
-//   res.status(201).send(customer);
-// });
+router.post(prefix, (req, res) => {
+  const newConfig = {
+    ...req.body,
+    createdAt: new Date().toISOString()
+  }
+  const customer = new Config(newConfig);
+  customer.save((err) => {
+    if (err) return err;
+  });
+  res.status(201).send(customer);
+});
 
 router.get(prefix, checkJwt, (req, res) => {
   Config.findOne(WHERE(req), (e, c) => {
@@ -57,7 +64,10 @@ router.put(prefix, checkJwt, async (req, res) => {
     update[`properties.${property}`] = req.body[property];
   }
 
-  await Config.findOneAndUpdate(WHERE(req), update, {new: true}, (e, c) => {
+  update.lastUpdated = new Date().toISOString();
+  update.updatedBy = req.user.sub;
+
+  await Config.findOneAndUpdate(WHERE(req), update, {new: true, useFindAndModify: false}, (e, c) => {
     if (e) return e;
     res.status(201).send(c);
   });
