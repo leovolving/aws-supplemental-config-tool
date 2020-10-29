163d0b660cbcef178aff01ab9244325167da73fe
Leo Yockey <leoyockey@gmail.com>
2020-10-29 13:15:53 -0700

    fix build

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,4 +1,5 @@
 import { h } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
 import { Auth0Provider } from "@auth0/auth0-react";
 
 import Home from './home';
@@ -11,18 +12,25 @@ const process = {
 	}
 }
 
-const App = () =>  (
-	<Auth0Provider
-    domain={`${process.env.AUTH0_NAME}.us.auth0.com`}
-    clientId={process.env.AUTH0_CLIENT_ID}
-	redirectUri={window.location.origin}
-	audience={`https://${process.env.AUTH0_NAME}.us.auth0.com/api/v2/`}
-    scope="read:current_user update:current_user_metadata"
-  >
-	<div id="app">
-		<Home path="/" />
-	</div>
-	</Auth0Provider>
-);
+const App = () => {
+	const [redirectUri, setRedirectUri] = useState(null);
+	useEffect(() => {
+		setRedirectUri(window.location.origin);
+	}, []);
+
+	return (
+		<Auth0Provider
+		domain={`${process.env.AUTH0_NAME}.us.auth0.com`}
+		clientId={process.env.AUTH0_CLIENT_ID}
+		redirectUri={redirectUri}
+		audience={`https://${process.env.AUTH0_NAME}.us.auth0.com/api/v2/`}
+		scope="read:current_user update:current_user_metadata"
+	>
+		<div id="app">
+			<Home path="/" />
+		</div>
+		</Auth0Provider>
+	);
+}
 
 export default App;
